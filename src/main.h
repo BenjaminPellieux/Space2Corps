3ec89a1c59e00f5204b3ecec23451bd00fbde6a7
Benjamin PELLIEUX <benjaminpellieux5@gmail.com>
2025-10-03 14:31:33 +0200

    UPDATE

---
+++ src/main.h
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+// #include <EEPROM.h>
+
+
+void app_main();
