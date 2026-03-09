ae4002821ebacf06f77356ac4b955adde05bda7f
Benjamin PELLIEUX <benjaminpellieux5@gmail.com>
2026-03-10 00:59:22 +0100

    UPDATE gource config

--- gour.sh
+++ gour.sh
@@ -1,7 +1,8 @@
 gource \
     -s 2.5 \
     -1280x920 \
-    --auto-skip-seconds .01 \
+    --auto-skip-seconds .1 \
+    --caption-file commitmsg.txt \
     --multi-sampling \
     --stop-at-end \
     --highlight-users \
@@ -13,5 +14,5 @@ gource \
     --font-size 25 \
     --font-colour FFFFFF \
     --output-ppm-stream - \
-    --output-framerate 60 \
-    | ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - -vcodec h264 -preset medium -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 ../movie.x264.avi
+    --output-framerate 30 \
+    | ffmpeg -y -r 30 -f image2pipe -vcodec ppm -i - -vcodec h264 -preset medium -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 movie.x264.avi
